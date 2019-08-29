f9311550d7ec82bf7ddec7e5094d34912780bf60
Dimitris Bliamplias <bl.dimitris@gmail.com>
2019-08-29 10:22:26 +0300

    Update date range on copyright notice

--- client.go
+++ client.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Skroutz S.A.
+// Copyright 2017-2019 Skroutz S.A.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 // Rafka: Kafka exposed with a Redis API
 //
-// Copyright 2017 Skroutz S.A.
+// Copyright 2017-2019 Skroutz S.A.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
